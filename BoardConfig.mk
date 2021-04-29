7d229c5bc8adf935826a00a117ec869df4ce5ac5
daniml3 <daniel@danielml.dev>
2021-04-29 19:19:36 +0200

    hanoip: initial support for included vendor and odm [2/2]
    * VINTF manifest, compatibility matrix and vendor properties from MIUI
    * Make QCOM_SOONG_NAMESPACE empty to opt-out from OSS QCOM HALs
    
    Change-Id: I72cd9d4efa8aa75851d9dc69bd156742545a5f27

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,6 +25,9 @@
 
 DEVICE_PATH := device/motorola/hanoip
 
+BUILD_BROKEN_DUP_RULES := true
+QCOM_SOONG_NAMESPACE := $(empty)
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a
@@ -114,6 +117,10 @@ $(foreach p, $(BOARD_PARTITION_LIST), $(eval BOARD_$(p)IMAGE_PARTITION_RESERVED_
 
 # Security patch level
 VENDOR_SECURITY_PATCH := 2022-03-01
+
+# Platform
+TARGET_BOARD_PLATFORM := $(TARGET_BOOTLOADER_BOARD_NAME)
+BOARD_USES_QCOM_HARDWARE := true
 
 # Recovery
 BOARD_USES_RECOVERY_AS_BOOT := true
@@ -220,7 +227,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compa
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_hanoip
 TARGET_RECOVERY_DEVICE_MODULES := libinit_hanoip
 
-#Properties
+# Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
